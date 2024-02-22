9e98d14d81f40ae1a1f2739fe6da789720469195
Jonas Heutz <j.heutz@brendow.de>
2024-02-22 14:12:07 +0100

    added Vite hot reload Server

---
+++ src/main.ts
@@ -0,0 +1,76 @@
+import './stylesheet.css'
+
+let userInput: string[] = [];
+const inputElement = document.getElementById("input") as HTMLInputElement;
+const list = document.createElement("ul") as HTMLUListElement;
+const delButton = document.getElementById("clear") as HTMLButtonElement;
+
+function makeUL(array: string[]) {
+    list.setAttribute("id", "ulist");
+    list.addEventListener("click", toggleClass);
+
+    for (let i = 0; i < userInput.length; i++) {
+        //create li item on document
+        const item = document.createElement("li");
+        console.log("item created")
+        //for each iteration of listitems append the corrosponding value in the crated li element
+        item.appendChild(document.createTextNode(userInput[i]));
+        //add the li elements to ul element
+        list.appendChild(item);
+    }
+    return list;
+}
+
+function deleteList() {
+    const ulist = document.getElementById("ulist");
+    userInput = [];
+    //while ul contains li elements, delete li element
+    while(ulist?.firstChild) {
+        ulist.removeChild(ulist.firstChild)
+    }
+}
+
+function toggleClass(ev: Event) {
+    const clickedItem = ev.target as HTMLElement;
+    if (clickedItem.tagName === "LI"){
+        clickedItem.classList.toggle("checked");
+    } 
+}
+
+const addButton = document.getElementById("add");
+addButton?.addEventListener("click", () => {
+    const userInputValue = inputElement.value;
+    if (userInputValue.trim() !== "") {
+        userInput.push(userInputValue);
+        const listdiv = document.getElementById("listdiv");
+        const ulist = document.getElementById("ulist");
+        if (listdiv?.contains(ulist) ){
+            const listElement = document.createElement("li");
+            //convert string to node element, so its assignable
+            listElement.textContent = inputElement.value;
+            list.appendChild(listElement);
+            inputElement.value="";
+        }
+        else {
+        listdiv?.appendChild(makeUL(userInput));
+        inputElement.value="";
+        }
+    }
+})
+
+delButton?.addEventListener("click", ()=> {
+    deleteList();
+})
+
+inputElement?.addEventListener("keydown", (ev) => {
+    if (ev.key === 'Enter'){
+        ev.preventDefault();
+        addButton?.click();
+    }
+})
+
+addEventListener("keydown", (ev: KeyboardEvent) => {
+    if (ev.key === "Delete"){
+        deleteList();
+    }
+})
