00a2855d6744ff53f59444ac8278703d21a11f8c
Jonas Heutz <j.heutz@brendow.de>
2024-05-16 15:16:43 +0200

    added some code explenation and fixed code structure

--- src/main.ts
+++ src/main.ts
@@ -4,40 +4,8 @@ let userInput: string[] = [];
 const inputElement = document.getElementById("input") as HTMLInputElement;
 const list = document.createElement("ul") as HTMLUListElement;
 const delButton = document.getElementById("clear") as HTMLButtonElement;
-
-function makeUL(array: string[]) {
-    list.setAttribute("id", "ulist");
-    list.addEventListener("click", toggleClass);
-
-    for (let i = 0; i < userInput.length; i++) {
-        //create li item on document
-        const item = document.createElement("li");
-        console.log("item created")
-        //for each iteration of listitems append the corrosponding value in the crated li element
-        item.appendChild(document.createTextNode(userInput[i]));
-        //add the li elements to ul element
-        list.appendChild(item);
-    }
-    return list;
-}
-
-function deleteList() {
-    const ulist = document.getElementById("ulist");
-    userInput = [];
-    //while ul contains li elements, delete li element
-    while(ulist?.firstChild) {
-        ulist.removeChild(ulist.firstChild)
-    }
-}
-
-function toggleClass(ev: Event) {
-    const clickedItem = ev.target as HTMLElement;
-    if (clickedItem.tagName === "LI"){
-        clickedItem.classList.toggle("checked");
-    } 
-}
-
 const addButton = document.getElementById("add");
+
 addButton?.addEventListener("click", () => {
     const userInputValue = inputElement.value;
     if (userInputValue.trim() !== "") {
@@ -58,10 +26,6 @@ addButton?.addEventListener("click", () => {
     }
 })
 
-delButton?.addEventListener("click", ()=> {
-    deleteList();
-})
-
 inputElement?.addEventListener("keydown", (ev) => {
     if (ev.key === 'Enter'){
         ev.preventDefault();
@@ -69,8 +33,46 @@ inputElement?.addEventListener("keydown", (ev) => {
     }
 })
 
+delButton?.addEventListener("click", ()=> {
+    deleteList();
+})
+
 addEventListener("keydown", (ev: KeyboardEvent) => {
     if (ev.key === "Delete"){
         deleteList();
     }
 })
+
+function makeUL(array: string[]) {
+    list.setAttribute("id", "ulist");
+    list.addEventListener("click", toggleClass);
+    
+    for (let i = 0; i < userInput.length; i++) {
+        //create li item on document
+        const item = document.createElement("li");
+        console.log("item created")
+        //for each iteration of listitems append the corrosponding value in the crated li element
+        item.appendChild(document.createTextNode(userInput[i]));
+        //add the li elements to ul element
+        list.appendChild(item);
+    }
+    return list;
+}
+
+
+function deleteList() {
+    const ulist = document.getElementById("ulist");
+    userInput = [];
+    //while ul contains li elements, delete li element
+    while(ulist?.firstChild) {
+        ulist.removeChild(ulist.firstChild)
+    }
+}
+
+//switch css class for striketrough of clicked li element
+function toggleClass(ev: Event) {
+    const clickedItem = ev.target as HTMLElement;
+    if (clickedItem.tagName === "LI"){
+        clickedItem.classList.toggle("linetrough");
+    } 
+}
